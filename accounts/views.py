1e1e8f50b8d23b1458760c58dda5bacc234ca710
redelectric90 <Paulandrewrichardson@msn.com>
2020-06-20 23:10:31 +0100

    Seeing if this will make website work for login

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def signup_view(request):
@@ -8,7 +8,15 @@ def signup_view(request):
         if form.is_valid():
             form.save()
             # add log in
-            return redirect('anime')           
+            return redirect('login')           
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html',{'form':form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+      form = AuthenticationForm()  
+    else:
+        form = AuthenticationForm()
+    return render (request, 'accounts/login.html',{'form':form})
